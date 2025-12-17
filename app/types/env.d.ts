279302a73e3cb310b26e8847b0cbf315d6a0ed47
wisdompan <wisdompan1@outlook.com>
2025-12-18 03:04:00 +0800

    环境变量更新

---
+++ app/types/env.d.ts
@@ -0,0 +1,10 @@
+declare module '@env' {
+  export const EXPO_PUBLIC_OPENAI_API_KEY: string;
+  export const EXPO_PUBLIC_GEMINI_API_KEY: string;
+  export const EXPO_PUBLIC_ENV: string;
+  export const EXPO_PUBLIC_DEV_API_URL: string;
+  export const EXPO_PUBLIC_PROD_API_URL: string;
+  export const EXPO_PUBLIC_BASE_URL: string;
+  export const EXPO_PUBLIC_OPENAI_MODEL: string;
+  export const EXPO_PUBLIC_GEMINI_MODEL: string;
+}
